fd5dafbfb0ff2039cbf2863716615b19054b0d4e
lsankar4033 <lsankar4033@gmail.com>
2022-06-07 09:49:32 -0700

    Add message signing to frontend

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,37 +7,39 @@ import { setupWeb3 } from "../lib/web3";
 import styles from "../styles/Home.module.css";
 
 import { merkleTree, addressInTree } from "../lib/merkleTree";
+import { buildInput } from "../lib/frontend/zkp";
 
 // TODO: this is a page for creating proofs + sending with message
 // UI 'steps':
 // 0. connect metamask
+// 1. sign data!
 // 1. generate proof
 // 2. (creating proof)
 // 3. submit proof + msg to server
-// 4. verified! tweet coming soon
+// 4. verified!
 
 // TODO: first testing with smaller proof, then test against the big guy...
-const getStep = (ethAddress: string | null) => {
-  if (!ethAddress || !addressInTree(ethAddress)) return 0;
-
-  // if no proof + not creating proof, return 1
-
-  // if no proof + creating proof return 2
-
-  // if proof exists but no submission yet return 3
-
-  // else 4
-
-  return 1;
-};
 
 const Home: NextPage = () => {
+  const [signer, setSigner] = useState<any | null>(null);
   const [metamaskAddress, setMetamaskAddress] = useState<string>("");
+
+  const [message, setMessage] = useState<string | null>(null);
+  const [signature, setSignature] = useState<string | null>(null);
+
+  const getStep = () => {
+    if (!metamaskAddress || !addressInTree(metamaskAddress)) return 0;
+
+    if (!message || !signature) return 1;
+
+    return 2;
+  };
 
   const connectToMetamask = () => {
     const connectToMetamaskAsync = async () => {
       const { provider, signer, network } = await setupWeb3();
       // console.log(provider, signer, network)
+      setSigner(signer);
       const addr = await signer.getAddress();
       console.log(`Connected address: ${addr}`);
       setMetamaskAddress(addr);
@@ -46,13 +48,34 @@ const Home: NextPage = () => {
   };
 
   const generateProof = () => {
-    // 1. get input
-    // incl. getting signature!
+    const input = buildInput(metamaskAddress);
+    // 1. gen input
     // 2. put input in generateProof
-    // TODO: store proof somewhere
+    // 3. store proof in state
   };
 
-  const step = getStep(metamaskAddress);
+  const signMessage = () => {
+    const signMessageAsync = async () => {
+      const domain = {
+        name: "dao hack gossip",
+        version: "1",
+        chainId: 1,
+      };
+      const types = {
+        Message: [{ name: "message", type: "string" }],
+      };
+      const value = {
+        message,
+      };
+
+      const signature = await signer._signTypedData(domain, types, value);
+      setSignature(signature);
+    };
+
+    signMessageAsync();
+  };
+
+  const step = getStep();
 
   return (
     <div className={styles.container}>
@@ -77,6 +100,24 @@ const Home: NextPage = () => {
         )}
 
         {step == 1 && (
+          <div>
+            <p className={styles.description}>Sign your message</p>
+
+            <input
+              type="text"
+              name="message"
+              onChange={(e) => setMessage(e.target.value)}
+            ></input>
+            <Button
+              disabled={message === null || message.length === 0}
+              onClick={signMessage}
+            >
+              Sign
+            </Button>
+          </div>
+        )}
+
+        {step == 2 && (
           <div>
             <p className={styles.description}>Generate proof</p>
 
