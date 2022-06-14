6f95e751a3ec2994053877d60a47be7e4c1b6cdc
lsankar4033 <lsankar4033@gmail.com>
2022-06-14 15:15:33 -0400

    Merkle root check on server

--- pages/api/verify.ts
+++ pages/api/verify.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { postToIpfs } from "../../lib/ipfs";
+import { merkleTree } from "../../lib/merkleTree";
 
 import { postTweet } from "../../lib/twitter";
 import { verifyProof } from "../../lib/zkp";
@@ -23,22 +24,24 @@ export default async function handler(
   const publicSignals = body.publicSignals;
   const message = body.message;
 
-  // TODO: it's super important that we test that the merkle root is legit here! Else they could create their own root...
+  if (publicSignals.root !== merkleTree.root) {
+    res.status(401).json("incorrect merkle root used");
+  } else {
+    const verified = await verifyProof(proof, publicSignals);
+    console.log(`Verification status: ${verified}`);
 
-  const verified = await verifyProof(proof, publicSignals);
-  console.log(`Verification status: ${verified}`);
+    // TODO: do we need error handling here?
+    const cid = await postToIpfs(JSON.stringify(proof));
+    console.log(`Posted to ipfs: ${cid.toString()}`);
 
-  // TODO: do we need error handling here?
-  const cid = await postToIpfs(JSON.stringify(proof));
-  console.log(`Posted to ipfs: ${cid.toString()}`);
-
-  if (verified) {
-    await postTweet(`${message}
+    if (verified) {
+      await postTweet(`${message}
     
 proof(ipfs): ${cid.toString()}`);
-    res.status(200).json({ ipfsHash: cid.toString() });
-  } else {
-    console.log(`Failed verification for proof ${proof}`);
-    res.status(401).json("failed verification");
+      res.status(200).json({ ipfsHash: cid.toString() });
+    } else {
+      console.log(`Failed verification for proof ${proof}`);
+      res.status(401).json("failed verification");
+    }
   }
 }
